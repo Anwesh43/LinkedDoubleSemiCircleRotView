b0666532e37df05da6db017df6f0494f870f2b56
Anwesh43 <anweshthecool0@gmail.com>
2019-02-07 11:18:43 +0530

    adding DSCRNode class

--- doublesemicirclerotview/src/main/java/com/anwesh/uiprojects/doublesemicirclerotview/DoubleSemiCircleRotView.kt
+++ doublesemicirclerotview/src/main/java/com/anwesh/uiprojects/doublesemicirclerotview/DoubleSemiCircleRotView.kt
@@ -122,4 +122,48 @@ class DoubleSemiCircleRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DSCRNode(var i : Int, val state : State = State()) {
+
+        private var next : DSCRNode? = null
+        private var prev : DSCRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = DSCRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDSCRNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DSCRNode {
+            var curr : DSCRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
