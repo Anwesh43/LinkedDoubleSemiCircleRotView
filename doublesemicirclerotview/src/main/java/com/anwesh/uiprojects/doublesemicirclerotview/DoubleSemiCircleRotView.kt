a1139c71cde2605d6765dd4de5e8ad6488012bbc
Anwesh43 <anweshthecool0@gmail.com>
2019-02-07 10:55:31 +0530

    adding Int and Float extension methods

--- doublesemicirclerotview/src/main/java/com/anwesh/uiprojects/doublesemicirclerotview/DoubleSemiCircleRotView.kt
+++ doublesemicirclerotview/src/main/java/com/anwesh/uiprojects/doublesemicirclerotview/DoubleSemiCircleRotView.kt
@@ -10,7 +10,7 @@ import android.content.Context
 import android.graphics.RectF
 import android.graphics.Paint
 import android.graphics.Canvas
-import android.graphics.Color 
+import android.graphics.Color
 
 val nodes : Int = 5
 val lines : Int = 5
@@ -19,3 +19,13 @@ val scDiv : Double = 0.51
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#FF6F00")
 val backColor : Int = Color.parseColor("#212121")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
