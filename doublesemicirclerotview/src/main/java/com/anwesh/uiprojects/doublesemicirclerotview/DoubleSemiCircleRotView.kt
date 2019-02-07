58c98a059a61ec2005d2da00df0ad39d21fe3813
Anwesh43 <anweshthecool0@gmail.com>
2019-02-07 10:52:50 +0530

    adding constants and importing libraries

--- doublesemicirclerotview/src/main/java/com/anwesh/uiprojects/doublesemicirclerotview/DoubleSemiCircleRotView.kt
+++ doublesemicirclerotview/src/main/java/com/anwesh/uiprojects/doublesemicirclerotview/DoubleSemiCircleRotView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.doublesemicirclerotview
 /**
  * Created by anweshmishra on 07/02/19.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color 
+
+val nodes : Int = 5
+val lines : Int = 5
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#FF6F00")
+val backColor : Int = Color.parseColor("#212121")
