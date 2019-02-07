8a0f5e543f9cb898ec5077bd54f597a6926342d3
Anwesh43 <anweshthecool0@gmail.com>
2019-02-07 11:29:59 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddoublesemicirclerotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddoublesemicirclerotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkeddoublesemicirclerotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.doublesemicirclerotview.DoubleSemiCircleRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DoubleSemiCircleRotView.create(this)
     }
 }
